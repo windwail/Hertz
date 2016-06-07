d43989740427137a6d0d0dd26d3fd9e518fb1c75
windwail <windwail@mail.ru>
2016-06-07 15:25:39 +0300

    update

--- app/src/main/java/checkmobile/de/hertz/StartInfleetActivity.java
+++ app/src/main/java/checkmobile/de/hertz/StartInfleetActivity.java
@@ -1,7 +1,12 @@
 package checkmobile.de.hertz;
 
+import android.content.Context;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class StartInfleetActivity extends AppCompatActivity {
 
