e33ec880aa24a98e64b8dc55bd5cc508478bd8e7
windwail <windwail@mail.ru>
2016-06-08 11:16:24 +0300

    android annotations

--- app/src/main/java/checkmobile/de/hertz/applicaton/MyApplication.java
+++ app/src/main/java/checkmobile/de/hertz/applicaton/MyApplication.java
@@ -3,12 +3,6 @@ package checkmobile.de.hertz.applicaton;
 import android.app.Application;
 
 import org.androidannotations.annotations.EApplication;
-import org.androidannotations.ormlite.annotations.OrmLiteDao;
-
-import checkmobile.de.hertz.dao.InfleetStartDao;
-import checkmobile.de.hertz.db.DatabaseHelper;
-import checkmobile.de.hertz.entity.InfleetStart;
-import lombok.Getter;
 
 @EApplication
 public class MyApplication extends Application{
